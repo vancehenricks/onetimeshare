a2f1553885b7373a079174142b72649fd68a3b36
Vance Henricks G. Patual <vpatualphone@gmail.com>
2026-05-05 03:51:49 +0800

    chore: update devDependencies and add testing setup
    - Added @testing-library/jest-dom and @testing-library/react for improved testing capabilities.
    - Included jest and ts-jest for TypeScript support in testing.
    - Added jest-environment-jsdom to provide a browser-like environment for tests.
    - Created setupTests.ts to configure testing library imports.

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+      },
+    }],
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/main.tsx',
+    '!src/vite-env.d.ts',
+  ],
+};
